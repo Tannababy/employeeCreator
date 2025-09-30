16da66d8d79336a10b4e5980fb93396726f4f007
Lotanna <tannaloamobi@gmail.com>
2025-09-30 23:50:16 +0100

    removed contractType object and set directly as field in employee model, added cross origin annotaion to resolve cors conflicts with front end vite app

--- src/main/java/com/projects/employeeCreator/controller/EmployeeController.java
+++ src/main/java/com/projects/employeeCreator/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.projects.employeeCreator.model.Employee;
 import com.projects.employeeCreator.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
--- src/main/java/com/projects/employeeCreator/model/Employee.java
+++ src/main/java/com/projects/employeeCreator/model/Employee.java
@@ -8,30 +8,101 @@ public class Employee {
     // ensures each employee gets a unique primary key automatically.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Integer id;
+
     private String firstName;
     private String lastName;
     private String email;
-    private int mobile;
+    private String mobile;
     private String address;
     private String department;
-    private ContractInfo contractInfo;
-
+    private String contractType;
+    private String startDate;
     // no arg constructor required for JPA
     public Employee() {
     }
 
-    public Employee(Integer id, String firstName, String lastName, int mobile, String email, String address, String department, ContractInfo contractInfo) {
-        this.id = id;
+    public Employee(String firstName, String lastName, String email, String mobile,
+                    String address, String department, String contractType, String startDate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.mobile = mobile;
         this.address = address;
         this.department = department;
-        this.contractInfo = contractInfo;
+        this.contractType = contractType;
+        this.startDate = startDate;
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
 }
